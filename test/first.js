306108c9f64bc315572cc958f952deae711abc2a
Yusaf Osei-Bonsu <yusaf.oseibonsu@centrica.com>
2020-12-31 16:21:15 +0000

    Setting a fake date & passing test

--- test/first.js
+++ test/first.js
@@ -1,6 +1,13 @@
 const assert = require("assert");
 const { greet } = require("../greeter.js");
+const { useFakeTimers } = require("sinon");
 
 describe("Testing the greeter", () => {
-    it("Checks the greet function", () => assert.strictEqual(greet("Alice"), "Hello, Alice! Today is Thursday, December 31, 2020")); 
+    it("Checks the greet function", () => {
+        let clock = useFakeTimers(new Date(2021, 0, 15));
+        assert.strictEqual(greet("Alice"), "Hello, Alice! Today is Friday, January 15, 2021");
+
+        // Time goes back to normal
+        clock.restore();
+    }); 
 });
